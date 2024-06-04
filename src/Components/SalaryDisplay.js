56c3d112bf72e37103ed101059bfb17419125050
Vibhujay99 <vibhujay99@gmail.com>
2024-06-04 19:40:46 +0530

    Ref() : Comments

--- src/Components/SalaryDisplay.js
+++ src/Components/SalaryDisplay.js
@@ -119,31 +119,6 @@ const SalaryDisplay = () => {
         </div>
       </Stack>
     </Stack>
-    // <div>
-    //   <h2>Salary Details</h2>
-    //   <p>Basic Salary: {basicSalary}</p>
-    //   <h3>Earnings</h3>
-    //   <ul>
-    //     {earnings.map((e, index) => (
-    //       <li key={index}>
-    //         {e.description}: {e.amount}
-    //       </li>
-    //     ))}
-    //   </ul>
-    //   <h3>Deductions</h3>
-    //   <ul>
-    //     {deductions.map((d, index) => (
-    //       <li key={index}>
-    //         {d.description}: {d.amount}
-    //       </li>
-    //     ))}
-    //   </ul>
-    //   <p>EPF: {epf}</p>
-    //   <p>ETF: {etf}</p>
-    //   <p>APIT: {apit}</p>
-    //   <p>Net Salary: {netSalary}</p>
-    //   <p>Cost to Company: {costToCompany}</p>
-    // </div>
   );
 };
 
